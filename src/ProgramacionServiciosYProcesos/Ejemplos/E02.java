7f8ff0e3df7edbfd07ae58139a93c96fc0a8b3e1
Ruper <ruper.tech9@gmail.com>
2024-09-26 10:59:11 +0200

    Carga

--- src/ProgramacionServiciosYProcesos/Ejemplos/E02.java
+++ src/ProgramacionServiciosYProcesos/Ejemplos/E02.java
@@ -6,20 +6,29 @@ import java.io.InputStream;
 public class E02 {
     public static void main(String[] args) throws IOException {
 
+        Process pb = new ProcessBuilder("CMD", "/C", "DIR").start();
         try {
-            Process pb = new ProcessBuilder("CMD", "/C", "DIR").start();
+
             InputStream is = pb.getInputStream();
 
+            // mostramos en pantalla caracter a caracter
             int c;
-            while ((c = is.read()) != -1){
+            while ((c = is.read()) != -1)
                 System.out.print((char)c);
-            }
             is.close();
+
         } catch (Exception e){
             e.printStackTrace();
-
         }
-    }
 
+    // COMPROBACION DE ERROR - 0 bien - 1 mal
+    int exitVal;
+		try {
+        exitVal = pb.waitFor();
+        System.out.println("Valor de Salida: " + exitVal);
+    } catch (InterruptedException e) {
+        e.printStackTrace();
+    }
+}
 }
 
