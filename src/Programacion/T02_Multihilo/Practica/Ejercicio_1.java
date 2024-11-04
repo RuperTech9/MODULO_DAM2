f01381a13c16030e9d85da67ad50348ab65480bf
Ruper <ruper.dam1@gmail.com>
2024-11-04 09:10:49 +0100

    Carga

--- src/Programacion/T02_Multihilo/Practica/Ejercicio_1.java
+++ src/Programacion/T02_Multihilo/Practica/Ejercicio_1.java
@@ -2,7 +2,7 @@ package Programacion.T02_Multihilo.Practica;
 
 public class Ejercicio_1 {
     public static void main(String[] args) {
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 30; i++) {
             TareaCalculo_b tarea =  new TareaCalculo_b("Hilo" + i);
             Thread hilo = new Thread(tarea);
             hilo.start();
