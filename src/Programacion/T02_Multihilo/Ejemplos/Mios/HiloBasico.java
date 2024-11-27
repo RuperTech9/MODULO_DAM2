a3a1f286ee67dbd6c228fc42487a17d715065324
Ruper <ruper.dam1@gmail.com>
2024-11-27 10:07:05 +0100

    Carga

--- src/Programacion/T02_Multihilo/Ejemplos/Mios/HiloBasico.java
+++
@@ -1,7 +0,0 @@
-package Programacion.T02_Multihilo.Ejemplos.Mios;
-
-public class HiloBasico extends Thread {
-    public void run() {
-        System.out.println("Hola desde el hilo");
-    }
-}
--- src/Programacion/T02_Multihilo/Ejemplos/Mios/HiloBasico_Main.java
+++
@@ -1,9 +0,0 @@
-package Programacion.T02_Multihilo.Ejemplos.Mios;
-
-public class HiloBasico_Main {
-    public static void main(String[] args) {
-        HiloBasico hilo = new HiloBasico();
-        hilo.start();
-    }
-}
-
