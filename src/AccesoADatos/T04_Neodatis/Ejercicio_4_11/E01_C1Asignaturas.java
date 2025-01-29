753fbceffe0e0cae0daa58814cd5aa8d9d231966
Ruper <ruper.dam1@gmail.com>
2025-01-29 12:12:11 +0100

    Carga

---
+++ src/AccesoADatos/T04_Neodatis/Ejercicio_4_11/E01_C1Asignaturas.java
@@ -0,0 +1,41 @@
+package AccesoADatos.T04_Neodatis.Ejercicio_4_11;
+
+import java.util.Set;
+
+public class E01_C1Asignaturas {
+
+    private String codAsig;
+    private String nombreAsi;
+    private Set<E01_C1Profesores> setprofesores;
+
+    public E01_C1Asignaturas(String codAsig, String nombreAsi, Set<E01_C1Profesores> setprofesores) {
+        super();
+        this.codAsig = codAsig;
+        this.nombreAsi = nombreAsi;
+        this.setprofesores = setprofesores;
+    }
+
+    public E01_C1Asignaturas(){}
+
+    //GETTERS y SETTERS
+
+    public String getCodAsig() {
+        return codAsig;
+    }
+    public void setCodAsig(String codAsig) {
+        this.codAsig = codAsig;
+    }
+    public String getNombreAsi() {
+        return nombreAsi;
+    }
+    public void setNombreAsi(String nombreAsi) {
+        this.nombreAsi = nombreAsi;
+    }
+    public Set<E01_C1Profesores> getSetprofesores() {
+        return setprofesores;
+    }
+    public void setSetprofesores(Set<E01_C1Profesores> setprofesores) {
+        this.setprofesores = setprofesores;
+    }
+
+}
